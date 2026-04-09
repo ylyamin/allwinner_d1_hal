8fb7a22c105343bf0091ae2890b83078195f065c
ylyamin <yrans1@gmail.com>
2026-04-09 21:06:56 +0700

    doom runs

--- src/components/doom_task.c
+++ src/components/doom_task.c
@@ -3,10 +3,15 @@
 #include <doom1_wad.h>
 #include <tlsf.h>
 #include <string.h>
+#include <stdio.h>
+#include <ccu.h>
 
 //#define DOOM_IMPLEMENTATION
 #include "DOOM.h"
+#include "doomdef.h"
 
+extern unsigned char __HeapBase; 
+extern unsigned char __HeapLimit; 
 static tlsf_t mem_pool = NULL;
 extern uint8_t fb1[]; 
 
@@ -24,11 +29,21 @@ void doom_free_fnc(void* ptr)
 	tlsf_free(mem_pool, ptr);
 }
 
+//xxd -i doom1.wad > doom1_wad.h
+
+int file_counter = 0;
+
 void* doom_open_fnc(const char* filename, const char* mode)
 {
-    LOG_W("%s",filename);
+    LOG_W("doom_open_fnc: %s",filename);
+    void* f;
     if(strcmp(filename,"/home/doom1.wad") == 0)
-        return 1;
+    {
+        file_counter = 0;
+        f = &doom1_wad;
+        LOG_W("f: %p",f);
+        return f;
+    }     
     else
         return 0;
 }
@@ -38,18 +53,18 @@ void doom_close_fnc(void* handle)
     LOG_W("doom_close_fnc");
 }
 
-
 int doom_read_fnc(void* handle, void *buf, int count)
 {
-    LOG_W("doom_read_fnc");
+    //LOG_W("doom_read_fnc handle %p, buf %p, count %d, file_counter %d",handle + file_counter,buf,count, file_counter);
 
-    src_lib_puredoom_doom1_wad_len -= count;
-    memcpy(buf, src_lib_puredoom_doom1_wad, count);
-
-    if (src_lib_puredoom_doom1_wad_len)
-        return 1;
-    else     
-        return -1;
+    if(file_counter <= doom1_wad_len)
+    {
+        memcpy(buf, handle + file_counter, count);
+        file_counter += count;
+        return count;
+    }
+    else    
+        return 0;
 }
 
 int doom_write_fnc(void* handle, const void *buf, int count)
@@ -59,18 +74,31 @@ int doom_write_fnc(void* handle, const void *buf, int count)
 }
 int doom_seek_fnc(void* handle, int offset, doom_seek_t origin)
 {
-    LOG_W("doom_seek_fnc");
-    return -1;
+    //LOG_W("doom_seek_fnc file_counter %d, offset %d, origin %d", file_counter, offset, origin);
+
+    if(origin == DOOM_SEEK_SET) file_counter = offset;
+    if(origin == DOOM_SEEK_CUR) file_counter += offset;
+    if(origin == DOOM_SEEK_END) file_counter = doom1_wad_len + offset;
+    return 0;
 }
 int doom_tell_fnc(void* handle)
 {
-    LOG_W("doom_tell_fnc");
-    return -1;
+    //LOG_W("doom_tell_fnc");
+    return file_counter;
 }
 int doom_eof_fnc(void* handle)
 {
     LOG_W("doom_eof_fnc");
-    return 1;
+    if(file_counter == doom1_wad_len)
+        return 1;
+    else
+        return 0;
+}    
+
+void doom_gettime_fnc(int* sec, int* usec)
+{
+    *sec = ( get_time_ms() / 1000 );
+    *usec = get_time_us();
 }
 
 char* doom_getenv_fnc(const char* var) 
@@ -81,8 +109,7 @@ char* doom_getenv_fnc(const char* var)
 
 void doom_task_init(void)
 {
-    extern unsigned char __HeapBase; 
-    extern unsigned char __HeapLimit; 
+
     size_t s = (size_t)(&__HeapLimit - &__HeapBase);
 	LOG_W("heap: creating mem pool @ %08x size %d\n", &__HeapBase, s);
 	mem_pool = tlsf_create_with_pool((void *)&__HeapBase, s);
@@ -96,9 +123,9 @@ void doom_task_init(void)
                       doom_seek_fnc,
                       doom_tell_fnc,
                       doom_eof_fnc);
+    doom_set_gettime(doom_gettime_fnc);
+    //doom_set_exit(doom_exit_fnc);
     doom_set_getenv(doom_getenv_fnc);
-
-
 
     char argv[0]; 
     doom_init(0, *argv, 0);
@@ -108,8 +135,8 @@ void doom_task_init(void)
 void doom_task_exec(void)
 {
     doom_update();
-    *fb1 = doom_get_framebuffer(4 /* RGBA */);
+    memcpy(&fb1, doom_get_framebuffer(3 /* RGBA */),  SCREENWIDTH * SCREENHEIGHT * 3);
+    LOG_W("s:%d, us:%d",( get_time_ms() / 1000), get_time_us );
 }
 
 
-//xxd -i src/lib/puredoom/doom1.wad > doom1.wad.h
